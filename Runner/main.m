848cf4aa368f76f86a96d5af6ec55b427f88d641
wxiubin <iosgg@outlook.com>
2019-07-14 00:06:46 +0800

    add runner target

---
+++ Runner/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Runner
+//
+//  Created by wxiubin on 2019/7/14.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
